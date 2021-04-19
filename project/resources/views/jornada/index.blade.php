cd019e47de47536e31ef56ddd6445653eb91d30a
GlinkIsCoding <carlos.espino.m@gmail.com>
2021-04-18 21:14:06 -0500

    Redireccionamiento de botón editar

--- project/resources/views/jornada/index.blade.php
+++ project/resources/views/jornada/index.blade.php
@@ -117,8 +117,8 @@
                 var tableRow = '';
                 $('#dynamic-row').html('');
                 $.each(res, function(index, value){
-                    var urledit = 'Jornada/'+value.id+'/edit';
-                    var urldel = 'Jornada/'+value.id;
+                    var urledit = 'jornada/'+value.id+'/edit';
+                    var urldel = 'jornada/'+value.id;
                     tableRow = '<tr><td>'+value.id+'</td><td>'+value.nombre+'</td><td>'+value.fecha+'</td><td>'+value.localidad+'</td><td>'+value.municipio+'</td>';
                     tableRow += '<td><a href="'+urledit+'" class="btn btn-warning">Editar</a>';
                     tableRow += '<form action="'+urldel+'" class="d-inline" method="post"><input type="submit" onclick="return confirm("¿Quieres borrar?")"  class="btn btn-danger" value="Borrar"></form>';
@@ -141,8 +141,8 @@
                 var tableRow = '';
                 $('#dynamic-row').html('');
                 $.each(res, function(index, value){
-                    var urledit = 'Jornada/'+value.id+'/edit';
-                    var urldel = 'Jornada/'+value.id;
+                    var urledit = 'jornada/'+value.id+'/edit';
+                    var urldel = 'jornada/'+value.id;
                     tableRow = '<tr><td>'+value.id+'</td><td>'+value.nombre+'</td><td>'+value.fecha+'</td><td>'+value.localidad+'</td><td>'+value.municipio+'</td>';
                     tableRow += '<td><a href="'+urledit+'" class="btn btn-warning">Editar</a>';
                     tableRow += '<form action="'+urldel+'" class="d-inline" method="post"><input type="submit" onclick="return confirm("¿Quieres borrar?")"  class="btn btn-danger" value="Borrar"></form>';
