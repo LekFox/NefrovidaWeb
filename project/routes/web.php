aaff3e91a51c2bd8cf2f135894947d84d434797c
Franciscodzv <franciscodzv@gmail.com>
2021-04-21 21:32:05 -0500

    Fixed sidebar

--- project/routes/web.php
+++ project/routes/web.php
@@ -36,3 +36,6 @@ Route::group(['middleware' => 'auth'],  function () {
 
 Route::post('/jornadas/search', ['as' => 'search-jornadas', 'uses' => 'App\Http\Controllers\JornadaController@searchJornadas']);
 Route::post('/jornadas/searchloc', ['as' => 'search-jornadas-loc', 'uses' => 'App\Http\Controllers\JornadaController@searchJornadasLoc']);
+
+Route::get('/usuarios','App\Http\Controllers\RegistrarUsuariosController@index');
+//Route::post('/register','App\Http\Controllers\RegistrationController@store');
