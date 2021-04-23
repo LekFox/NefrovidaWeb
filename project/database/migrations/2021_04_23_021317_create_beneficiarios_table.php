0307cf41570d0ff0015b9c63a6b06c507a7351d8
GlinkIsCoding <carlos.espino.m@gmail.com>
2021-04-23 00:39:35 -0500

    Añadir migration de beneficiarios y de escolaridades, uso en tabla de jornadas

---
+++ project/database/migrations/2021_04_23_021317_create_beneficiarios_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBeneficiariosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('beneficiarios', function (Blueprint $table) {
+            $table->id();
+            $table->string("nombreBeneficiario", 140);
+            $table->date("fechaNacimiento");
+            $table->string("sexo", 10);
+            $table->string("telefono", 15);
+            $table->string("direccion", 200);
+            $table->foreignId("escolaridade_id")
+                ->nullable()
+                ->constrained()
+                ->onUpdate('cascade')
+                ->onDelete('set null');
+            $table->string("estatus", 50);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('beneficiarios');
+    }
+}
