aab73e20893849f3be650e52909f08b1b487fa86
GlinkIsCoding <carlos.espino.m@gmail.com>
2021-05-06 02:45:04 -0500

    Hotfix, sidebar jornada registrar usuario, faltó resolucionar conflicto

--- project/resources/views/sidebar/jornada.blade.php
+++ project/resources/views/sidebar/jornada.blade.php
@@ -13,11 +13,7 @@
             <a href="{{url('/beneficiario')}}" class="list-group-item list-group-item-action bg-light">Beneficiarios</a>
             <a href="#" class="list-group-item list-group-item-action bg-light">Comparar Jornadas</a>
             <a href="#" class="list-group-item list-group-item-action bg-light">Reportes</a>
-<<<<<<< HEAD
             <a href="{{url('/register')}}" class="list-group-item list-group-item-action bg-light">Registrar Usuario</a>
-=======
-            <a href="{{url('/usuarios')}}" class="list-group-item list-group-item-action bg-light ">Registrar Usuario</a>
->>>>>>> usuarios
         </div>
         </div>
         <!-- /#sidebar-wrapper -->
