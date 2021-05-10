93e2685e6dfd6a6aca4dc24cc50af4768b973ec0
carlosam2 <a01703682@itesm.mx>
2021-05-10 10:54:40 -0500

    sidebar color

--- project/resources/views/sidebar/home.blade.php
+++ project/resources/views/sidebar/home.blade.php
@@ -7,7 +7,7 @@
         <!-- Sidebar -->
         <div class="bg-light border-right" id="sidebar-wrapper">
         <div class="list-group list-group-flush">
-            <a href="{{url('/home')}}" class="list-group-item list-group-item-action bg-light active"><i class="bi bi-house-door-fill"></i> Inicio</a>
+            <a href="{{url('/home')}}" class="list-group-item list-group-item-action bg-success active"><i class="bi bi-house-door-fill"></i> Inicio</a>
             <a href="{{url('/jornada')}}" class="list-group-item list-group-item-action bg-light"><i class="bi bi-calendar3"></i> Jornadas</a>
             <a href="{{url('/beneficiario')}}" class="list-group-item list-group-item-action bg-light"><i class="bi bi-people-fill"></i> Beneficiarios</a>
             <a href="#" class="list-group-item list-group-item-action bg-light"><i class="bi bi-arrow-left-right"></i> Comparar Jornadas</a>
