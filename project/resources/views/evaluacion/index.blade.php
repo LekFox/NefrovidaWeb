5d13cbfc992cb0b9f5bc2346fe8b114cb8ea8d10
Franciscodzv <franciscodzv@gmail.com>
2021-05-09 21:01:07 -0500

    Added three dots

--- project/resources/views/evaluacion/index.blade.php
+++ project/resources/views/evaluacion/index.blade.php
@@ -1,7 +1,12 @@
 <br><br>
 <div class="">
 <div class="card">
-  <div class="card-body">
+  <div class="card-body text-right">
+    <a href="{{ url('notas/create') }}">
+      <svg xmlns="http://www.w3.org/2000/svg" href="{{ url('notas/create') }}" width="30" height="30" fill="gray" class="bi bi-three-dots-vertical" viewBox="0 0 16 16">
+        <path d="M9.5 13a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z"/>
+      </svg>
+    </a>
       <div class= "row">
           <div class= "col-sm">
             </div>
@@ -11,10 +16,10 @@
             <div class="col-sm">
             </div>
         </div>
-        
         <div class= "row">
           <div class= "col-sm">
             </div>
+            
             <div class="col-sm text-center">
               <div class="dropdown">
                 <button class="btn btn-primary btn-lg dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
