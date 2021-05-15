e9bc0e79637c302c227a36b43aa54abfabb6ad0a
GlinkIsCoding <carlos.espino.m@gmail.com>
2021-05-15 04:22:42 -0500

    Registrar Antecedentes completado (sin formato ni íconos)

--- project/app/Models/Antecedente.php
+++ project/app/Models/Antecedente.php
@@ -9,6 +9,13 @@ class Antecedente extends Model
 {
     use HasFactory;
 
+    protected $fillable =[
+        'casa', 'serviciosBasicos', 'beneficiario_id', 'serviciosBasicos', 'personalesPatologicos',
+        'personalesNoPatologicos', 'padreVivo', 'enfermedadesPadre', 'madreVivo', 'enfermedadesMadre',
+        'numHermanos', 'numHermanosVivos', 'enfermedadesHermanos', 'otrosHermanos', 'menarquia',
+        'ritmo', 'fum', 'gestaciones', 'partos', 'abortos', 'cesareas', 'ivsa', 'metodosAnticonceptivos',
+    ];
+
     public function beneficiario()
     {
         return $this->belongsTo(Beneficiario::class);
--- project/resources/views/antecedentes/create.blade.php
+++ project/resources/views/antecedentes/create.blade.php
@@ -28,92 +28,251 @@
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
 
   
-  <h1 id="AntecedentesTitulo">Registrar Antecedentes</h1>
+  <h1 id="AntecedentesTitulo" class="text-center">Registrar Antecedentes</h1>
+  <a href="{{ url()->previous() }}" class="btn btn-primary"> Regresar </a>
+  
   
   <div class="container mt-3">
     <div class="stepwizard col">
         <div class="stepwizard-row setup-panel">
             <div class="stepwizard-step">
                 <a href="#step-1" type="button" class="btn btn-primary btn-circle">1</a>
-                <p>Step 1</p>
+                <p>Datos de Vivienda</p>
             </div>
             <div class="stepwizard-step">
                 <a href="#step-2" type="button" class="btn btn-default btn-circle" disabled="disabled">2</a>
-                <p>Step 2</p>
+                <p>Antecedentes Personales</p>
             </div>
             <div class="stepwizard-step">
                 <a href="#step-3" type="button" class="btn btn-default btn-circle" disabled="disabled">3</a>
-                <p>Step 3</p>
+                <p>Antecedentes Familiares</p>
             </div>
             <div class="stepwizard-step">
                 <a href="#step-4" type="button" class="btn btn-default btn-circle" disabled="disabled">4</a>
-                <p>Step 4</p>
+                <p>Antecedentes Gineco-obstétricos</p>
             </div>
         </div>
     </div>
     <form role="form" action="" method="post">
         <div class="row setup-content" id="step-1">
-            <div class="col-xs-6 col-md-offset-3">
-                <div class="col-md-12">
-                    <h3> Step 1</h3>
-                    <div class="form-group">
-                        <label class="control-label">First Name</label>
-                        <input maxlength="100" type="text" class="form-control" placeholder="Enter First Name">
+            <div class="col">
+                
+            </div>
+            <div class="col">
+                <div class="col">
+                    <h3 class="text-center">Datos de Vivienda</h3>
+                    <br>
+                    <input type="hidden" id="beneficiario_id" name="beneficiario_id" value="{{ $beneficiario->id }}">
+                    <h5>Vive en casa:</h5>
+                    <div class="form-check">
+                      <input class="form-check-input" type="radio" name="casa" id="casa" value="propia" checked>
+                      <label class="form-check-label" for="propia">
+                        Propia
+                      </label>
                     </div>
-                    <div class="form-group">
-                        <label class="control-label">Last Name</label>
-                        <input maxlength="100" type="text" class="form-control" placeholder="Enter Last Name">
+                    <div class="form-check">
+                      <input class="form-check-input" type="radio" name="casa" id="casa" value="rentada">
+                      <label class="form-check-label" for="rentada">
+                        Rentada
+                      </label>
                     </div>
-                    <div class="form-group">
-                        <label class="control-label">Address</label>
-                        <textarea class="form-control" placeholder="Enter your address"></textarea>
+                    <div class="form-check">
+                      <input class="form-check-input" type="radio" name="casa" id="casa" value="prestada">
+                      <label class="form-check-label" for="prestada">
+                        Prestada
+                      </label>
                     </div>
-                    <button class="btn btn-primary nextBtn btn-lg pull-right" type="button">Next</button>
+                    <br>
+                    <h5>Servicios básicos:</h5>
+                    <div class="form-check">
+                      <input class="form-check-input" type="radio" name="serviciosBasicos" id="serviciosBasicos" value="1" checked>
+                      <label class="form-check-label" for="si">
+                        Sí
+                      </label>
+                    </div>
+                    <div class="form-check">
+                      <input class="form-check-input" type="radio" name="serviciosBasicos" id="serviciosBasicos" value="0">
+                      <label class="form-check-label" for="no">
+                        No
+                      </label>
+                    </div>
+                    <br>
+                    <div class="text-center">
+                      <button class="btn btn-primary nextBtn btn-lg pull-right" type="button">Next</button>
+                    </div>
                 </div>
+            </div>
+            <div class="col">
             </div>
         </div>
         <div class="row setup-content" id="step-2">
-            <div class="col-xs-6 col-md-offset-3">
-                <div class="col-md-12">
-                    <h3> Step 2</h3>
-                    <div class="form-group">
-                        <label class="control-label">Company Name</label>
-                        <input maxlength="200" type="text" required="required" class="form-control" placeholder="Enter Company Name">
-                    </div>
-                    <div class="form-group">
-                        <label class="control-label">Company Address</label>
-                        <input maxlength="200" type="text" required="required" class="form-control" placeholder="Enter Company Address">
-                    </div>
-                    <button class="btn btn-primary prevBtn btn-lg pull-left" type="button">Previous</button>
-                    <button class="btn btn-primary nextBtn btn-lg pull-right" type="button">Next</button>
+          <div class="col">
                 </div>
-            </div>
+                <div class="col">
+                    <div class="col">
+                        <h3 class="text-center">Antecedentes Personales</h3>
+                        <br>
+                        <h5>Personales Patológicos:</h5>
+                        <div class="form-group">
+                          <textarea class="form-control" id="personalesPatologicos" name="personalesPatologicos" maxlength="400" rows="3"></textarea>
+                        </div>
+                        <br>
+                        <h5>Personales No Patológicos:</h5>
+                        <div class="form-group">
+                          <textarea class="form-control" id="personalesNoPatologicos" name="personalesNoPatologicos" maxlength="400" rows="3"></textarea>
+                        </div>
+                        <br>
+                        <div class="text-center">
+                          <button class="btn btn-primary prevBtn btn-lg pull-left" type="button">Previous</button>
+                          <button class="btn btn-primary nextBtn btn-lg pull-right" type="button">Next</button>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                </div>
         </div>
         <div class="row setup-content" id="step-3">
-            <div class="col-xs-6 col-md-offset-3">
-                <div class="col-md-12">
-                    <h3> Step 3</h3>
-                    <button class="btn btn-primary prevBtn btn-lg pull-left" type="button">Previous</button>
-                    <button class="btn btn-primary nextBtn btn-lg pull-right" type="button">Next</button>
+          <div class="col">
                 </div>
-            </div>
+                <div class="col">
+                    <div class="col">
+                        <h3 class="text-center">Antecedentes Familiares</h3>
+                        <br>
+                        <h5>Padre vivo:</h5>
+                        <div class="form-check">
+                          <input class="form-check-input" type="radio" name="padreVivo" id="padreVivo" value="1" checked>
+                          <label class="form-check-label" for="si">
+                            Sí
+                          </label>
+                        </div>
+                        <div class="form-check">
+                          <input class="form-check-input" type="radio" name="padreVivo" id="padreVivo" value="0">
+                          <label class="form-check-label" for="no">
+                            No
+                          </label>
+                        </div>
+                        <br>
+                        <h5>Enfermedades del padre:</h5>
+                        <div class="form-group">
+                          <textarea class="form-control" id="enfermedadesPadre" name="enfermedadesPadre" maxlength="400" rows="3"></textarea>
+                        </div>
+                        <br>
+                        <h5>Madre viva:</h5>
+                        <div class="form-check">
+                          <input class="form-check-input" type="radio" name="madreVivo" id="madreVivo" value="1" checked>
+                          <label class="form-check-label" for="si">
+                            Sí
+                          </label>
+                        </div>
+                        <div class="form-check">
+                          <input class="form-check-input" type="radio" name="madreVivo" id="madreVivo" value="0">
+                          <label class="form-check-label" for="no">
+                            No
+                          </label>
+                        </div>
+                        <br>
+                        <h5>Enfermedades de la madre:</h5>
+                        <div class="form-group">
+                          <textarea class="form-control" id="enfermedadesMadre" name="enfermedadesMadre" maxlength="400" rows="3"></textarea>
+                        </div>
+                        <br>
+                        <h5>Número de hermanos:</h5>
+                        <div class="form-group">
+                          <input type="number" class="form-control" id="numHermanos" name="numHermanos" placeholder="Número de hermanos">
+                        </div>
+                        <br>
+                        <h5>Número de hermanos vivos:</h5>
+                        <div class="form-group">
+                          <input type="number" class="form-control" id="numHermanosVivos" name="numHermanosVivos" placeholder="Número de hermanos vivos">
+                        </div>
+                        <br>
+                        <h5>Enfermedades de los hermanos:</h5>
+                        <div class="form-group">
+                          <textarea class="form-control" id="enfermedadesHermanos" name="enfermedadesHermanos" maxlength="400" rows="3"></textarea>
+                        </div>
+                        <br>
+                        <h5>Observaciones de los hermanos:</h5>
+                        <div class="form-group">
+                          <textarea class="form-control" id="otrosHermanos" name="otrosHermanos" maxlength="400" rows="3"></textarea>
+                        </div>
+                        <div class="text-center">
+                          <button class="btn btn-primary prevBtn btn-lg pull-left" type="button">Previous</button>
+                          <button class="btn btn-primary nextBtn btn-lg pull-right" type="button">Next</button>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                </div>
+          </div>
         </div>
         <div class="row setup-content" id="step-4">
-            <div class="col-xs-6 col-md-offset-3">
-                <div class="col-md-12">
-                    <h3> Step 4</h3>
-                    <button class="btn btn-primary prevBtn btn-lg pull-left" type="button">Previous</button>
-                    <button class="btn btn-success btn-lg pull-right" type="submit">Submit</button>
-                </div>
-            </div>
+              <div class="col">
+              </div>
+              <div class="col">
+                  <div class="col">
+                      <h3 class="text-center">Antecedentes Gineco-obstétricos</h3>
+                      <br>
+                      <h5>Menarquia:</h5>
+                        <div class="form-group">
+                          <input type="number" class="form-control" id="menarquia" name="menarquia" placeholder="Edad de comienzo de menarquia">
+                        </div>
+                        <br>
+                      <h5>Ritmo:</h5>
+                        <div class="form-group">
+                            <input type="number" class="form-control" id="ritmo" name="ritmo" placeholder="Ritmo">
+                        </div>
+                      <br>
+                      <h5>F.U.M.:</h5>
+                        <div class="form-group">
+                            <input class="date form-control" type="date" name="fum" id="fum">    
+                        </div>
+                      <br>
+                      <h5>Gestaciones:</h5>
+                        <div class="form-group">
+                            <input type="number" class="form-control" id="gestaciones" name="gestaciones" placeholder="Número de gestaciones">
+                        </div>
+                      <br>
+                      <h5>Partos:</h5>
+                        <div class="form-group">
+                            <input type="number" class="form-control" id="partos" name="partos" placeholder="Número de partos">
+                        </div>
+                      <br>
+                      <h5>Abortos:</h5>
+                        <div class="form-group">
+                            <input type="number" class="form-control" id="abortos" name="abortos" placeholder="Número de abortos">
+                        </div>
+                      <br>
+                      <h5>Cesáreas:</h5>
+                        <div class="form-group">
+                            <input type="number" class="form-control" id="cesareas" name="cesareas" placeholder="Número de cesáreas">
+                        </div>
+                      <br>
+                      <h5>I.V.S.A.:</h5>
+                        <div class="form-group">
+                            <input type="number" class="form-control" id="ivsa" name="ivsa" placeholder="Inicio de vida sexual activa">
+                        </div>
+                      <br>
+                      <h5>Métodos anticonceptivos:</h5>
+                        <div class="form-group">
+                          <textarea class="form-control" id="metodosAnticonceptivos" name="metodosAnticonceptivos" maxlength="400" rows="3"></textarea>
+                        </div>
+                      <br>
+                      <div class="text-center">
+                        <button class="btn btn-primary prevBtn btn-lg pull-left" type="button">Previous</button>
+                          <button class="btn btn-success btn-lg pull-right" type="submit">Submit</button>
+                      </div>
+                  </div>
+              </div>
+              <div class="col">
+              </div>
         </div>
     </form>
 </div>
 
  
 
-  
-  <a href="{{ url()->previous() }}" class="btn btn-primary"> Regresar </a>
+  <br>
+
   
   <script type="text/javascript">
       $('.date').datepicker({  
