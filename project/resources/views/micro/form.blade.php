c3518b65b8ec508f85684483004156a8cbe3732c
MIguel Angel Marines Olvera <A01705317@itesm.mx>
2021-05-26 01:10:36 -0500

    Formulario Terminado

--- project/resources/views/micro/form.blade.php
+++ project/resources/views/micro/form.blade.php
@@ -13,6 +13,8 @@
 
    
 <h1 id="AntecedentesTitulo" class="text-center bluenefro"><i class="bi bi-eyedropper"></i> {{$mode}} Microalbuminuría de {{ $beneficiario->nombreBeneficiario }}</h1>
+<br>
+<br>
 <a href="{{ url('/beneficiario/'.$beneficiario->id.'/analisislab') }}" class="btn btn-primary"><i class="bi bi-arrow-left"></i> Regresar </a>
 <br>
 <br>
@@ -24,9 +26,9 @@
     </div>
     <div class="col-2">
     </div>
-    <div class="col-4">
+    <div class="col-3">
     <div class="d-flex justify-content-center">
-        <p class="font-weight-bold">Valores de referencia</p>
+        <p class="font-weight-bold">Valores de Referencia</p>
     </div>
     </div>
 </div>
@@ -71,6 +73,7 @@
 
 <br>
 <br>
+<br>
 
 <div class="form-row">
     <div class="col-4">
@@ -111,11 +114,12 @@
 
 <br>
 <br>
-   
+<br>
+
 
 <div class="form-row">
     <div class="col-4">
-        <label for="nombre">Albumina/Creatinina</label>
+        <label for="nombre">Microalbumina/Creatinina</label>
     </div>
     <div class="col-2">
     </div>
@@ -124,89 +128,89 @@
 </div>
 <div class="form-row">
     <div class="col-4">
-        <input type="number" class="form-control" placeholder="Albumina/Creatinina" id="microalbuminaCreatinina" name="microalbuminaCreatinina" >
+        <input type="number" class="form-control" placeholder="Microalbumina/Creatinina" id="microalbuminaCreatinina" name="creatinina">
     </div>
     <div class="col-2">
-    </div>
-    <div class="col-4">
         <div class="d-flex justify-content-center">
             <p class="font-weight-bold">Normal</p>
         </div>
+    </div>
+    <div class="col-4">
         <div class="d-flex justify-content-center">
-            <div class="form-group row">
-                <div class="col-3">
-                    <input class="form-control" type="text" placeholder="0.00" disabled>
+            <div class="form-row">
+                <div class="col-4">
+                    <input class="form-control" type="text" placeholder="30.00" disabled>
                 </div>
-                <div class="col-0">
+                <div class="col-1">
                     <div class="d-flex justify-content-center">
                         <p>-</p>
                     </div>
                 </div>
-                <div class="col-3">
-                    <input class="form-control" type="text" placeholder="30.00" disabled>
+                <div class="col-4">
+                    <input class="form-control" type="text" placeholder="300.00" disabled>
                 </div>
-                <div class="col-5">
+                <div class="col-3">
                     <p>mg/g</p>
                 </div>
             </div>
         </div>
     </div>
-</div>
-
-<div class="form-row">
+    <br>
+    <br>
+    
     <div class="col-4">
-    <div class="d-flex justify-content-center">
-        <p class="font-weight-bold">Anormal</p>
-    </div>
     </div>
     <div class="col-2">
+        <div class="d-flex justify-content-center">
+            <p class="font-weight-bold">Anormal</p>
+        </div>
     </div>
     <div class="col-4">
         <div class="d-flex justify-content-center">
-            <div class="form-group row">
-                <div class="col-3">
+            <div class="form-row">
+                <div class="col-4">
                     <input class="form-control" type="text" placeholder="30.00" disabled>
                 </div>
-                <div class="col-0">
+                <div class="col-1">
                     <div class="d-flex justify-content-center">
                         <p>-</p>
                     </div>
                 </div>
-                <div class="col-3">
+                <div class="col-4">
                     <input class="form-control" type="text" placeholder="300.00" disabled>
                 </div>
-                <div class="col-5">
+                <div class="col-3">
                     <p>mg/g</p>
                 </div>
             </div>
         </div>
     </div>
-</div>
+    <br>
+    <br>
 
-<div class="form-row">
     <div class="col-4">
-    <div class="d-flex justify-content-center">
-        <p class="font-weight-bold">Anormal Alto</p>
-    </div>
     </div>
     <div class="col-2">
+        <div class="d-flex justify-content-center">
+            <p class="font-weight-bold">Anormal</p>
+        </div>
     </div>
     <div class="col-4">
         <div class="d-flex justify-content-center">
-            <div class="form-group row">
-                <div class="col-3">
+            <div class="form-row">
+                <div class="col-4">
                     <input class="form-control" type="text" placeholder="300.00" disabled>
                 </div>
-                <div class="col-0">
+                <div class="col-1">
                     <div class="d-flex justify-content-center">
                         <p>-</p>
                     </div>
                 </div>
-                <div class="col-3">
+                <div class="col-4">
                     <input class="form-control" type="text" placeholder="Mayor" disabled>
                 </div>
-                <div class="col-5">
-                    <p>mg/g</p>
+                <div class="col-3">
+                    <p>mg/dL</p>
                 </div>
             </div>
         </div>
@@ -215,8 +219,7 @@
 
 <br>
 <br>
-
-
+<br>
 
 <div class="form-row">
     <div class="col-4">
@@ -242,10 +245,11 @@
 
 <br>
 <br>
+<br>
 
 <label for="comentario">Nota</label>
 <div class="form-group">
-    <textarea class="form-control" id="nota" name="nota" maxlength="200" rows="2"></textarea>
+    <textarea class="form-control" placeholder="Nota" id="nota" name="nota" maxlength="200" rows="5"></textarea>
 </div>
 
 <br>
