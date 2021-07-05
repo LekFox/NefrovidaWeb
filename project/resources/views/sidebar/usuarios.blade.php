2a31ea159996157e9c59aebbc5fab289e13f46d3
Franciscodzv <franciscodzv@gmail.com>
2021-07-05 13:23:57 -0500

    Final RBAC Fixes

--- project/resources/views/sidebar/usuarios.blade.php
+++ project/resources/views/sidebar/usuarios.blade.php
@@ -12,7 +12,15 @@
             <a href="#" class="list-group-item list-group-item-action bg-light"><i class="bi bi-arrow-left-right"></i> Comparar Jornadas</a>
             <a href="#" class="list-group-item list-group-item-action bg-light"><i class="bi bi-bar-chart-line-fill"></i> Reportes</a>
             {{-- <a href="{{url('/usuarios')}}" class="list-group-item list-group-item-action bg-success active"><i class="bi bi-person-plus-fill"></i> Registrar Usuario</a> --}}
-            @can('create', App\Models\User::class)
+            @can('social', App\Models\User::class)
+            <a href="{{url('/register')}}" class="list-group-item list-group-item-action bg-success active"><i class="bi bi-person-plus-fill"></i> Gestión de Usuarios</a>
+            @endcan
+
+            @can('admin', App\Models\User::class)
+            <a href="{{url('/register')}}" class="list-group-item list-group-item-action bg-success active"><i class="bi bi-person-plus-fill"></i> Gestión de Usuarios</a>
+            @endcan
+
+            @can('procuracion', App\Models\User::class)
             <a href="{{url('/register')}}" class="list-group-item list-group-item-action bg-success active"><i class="bi bi-person-plus-fill"></i> Gestión de Usuarios</a>
             @endcan
 
